61a2f88d396874ffb586a0b77a618045857e28c3
alexmanes83 <alexmanes83@gmail.com>
2025-02-02 09:58:13 -0300

    teste_unitário

---
+++ AuthApiCore.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace AuthApiCore.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
