9a2b61d186cec9a8472bcf5282fd754229422731
alexmanes83 <alexmanes83@gmail.com>
2025-02-02 13:58:05 -0300

    inicio teste unitário

--- AuthApiCore.Tests/UnitTest1.cs
+++ AuthApiCore.Tests/UnitTest1.cs
@@ -46,75 +46,75 @@ namespace AuthApiCore.Tests
             result.Should().Contain("Token");
         }
 
-        [Fact]
-        public async Task Login_InvalidUser_ReturnsUnauthorized()
-        {
-            // Arrange
-            var loginRequest = new
-            {
-                Email = "invalid_user",
-                Password = "wrong_password"
-            };
+        //[Fact]
+        //public async Task Login_InvalidUser_ReturnsUnauthorized()
+        //{
+        //    // Arrange
+        //    var loginRequest = new
+        //    {
+        //        Email = "invalid_user",
+        //        Password = "wrong_password"
+        //    };
 
-            // Act
-            var response = await _client.PostAsync("/api/auth/login", CreateJsonContent(loginRequest));
+        //    // Act
+        //    var response = await _client.PostAsync("/api/auth/login", CreateJsonContent(loginRequest));
 
-            // Assert
-            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
-        }
+        //    // Assert
+        //    response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        //}
 
-        [Fact]
-        public async Task CreateUser_ValidUser_ReturnsCreated()
-        {
-            // Arrange
-            var newUser = new RegisterModel { Id = 1, Email = "teste1234556@gmail.com", Password = "Password123" };
+        //[Fact]
+        //public async Task CreateUser_ValidUser_ReturnsCreated()
+        //{
+        //    // Arrange
+        //    var newUser = new RegisterModel { Id = 1, Email = "teste1234556@gmail.com", Password = "Password123" };
 
-            // Act
-            var response = await _client.PostAsync("api/auth/register", CreateJsonContent(newUser));
+        //    // Act
+        //    var response = await _client.PostAsync("api/auth/register", CreateJsonContent(newUser));
 
-            // Assert
-            response.StatusCode.Should().Be(HttpStatusCode.Created);
-        }
+        //    // Assert
+        //    response.StatusCode.Should().Be(HttpStatusCode.Created);
+        //}
 
-        [Fact]
-        public async Task UpdateUser_ValidUser_ReturnsNoContent()
-        {
-            // Arrange
-            var updatedUser = new RegisterModel { Email = "teste1234556@gmail.com", Password = "Aftc2680inpu@" };
+        //[Fact]
+        //public async Task UpdateUser_ValidUser_ReturnsNoContent()
+        //{
+        //    // Arrange
+        //    var updatedUser = new RegisterModel { Email = "teste1234556@gmail.com", Password = "Aftc2680inpu@" };
 
-            // Act
-            var response = await _client.PutAsync("/api/auth/1", CreateJsonContent(updatedUser));
+        //    // Act
+        //    var response = await _client.PutAsync("/api/auth/1", CreateJsonContent(updatedUser));
 
-            // Assert
-            response.StatusCode.Should().Be(HttpStatusCode.NoContent);
-        }
+        //    // Assert
+        //    response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        //}
 
-        [Fact]
-        public async Task DeleteUser_ValidUser_ReturnsNoContent()
-        {
-            // Act
-            var response = await _client.DeleteAsync("/api/auth/1");
+        //[Fact]
+        //public async Task DeleteUser_ValidUser_ReturnsNoContent()
+        //{
+        //    // Act
+        //    var response = await _client.DeleteAsync("/api/auth/1");
 
-            // Assert
-            response.StatusCode.Should().Be(HttpStatusCode.NoContent);
-        }
+        //    // Assert
+        //    response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        //}
 
-        [Fact]
-        public async Task Logout_ShouldReturnOk()
-        {
-            // Act
-            var response = await _client.PostAsync("/api/auth/logout", null);
+        //[Fact]
+        //public async Task Logout_ShouldReturnOk()
+        //{
+        //    // Act
+        //    var response = await _client.PostAsync("/api/auth/logout", null);
 
-            // Assert
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
-            var responseBody = await response.Content.ReadAsStringAsync();
-            responseBody.Should().Be("Logged out successfully");
-        }
+        //    // Assert
+        //    response.StatusCode.Should().Be(HttpStatusCode.OK);
+        //    var responseBody = await response.Content.ReadAsStringAsync();
+        //    responseBody.Should().Be("Logged out successfully");
+        //}
 
-        private StringContent CreateJsonContent(object obj)
-        {
-            var json = JsonConvert.SerializeObject(obj);
-            return new StringContent(json, Encoding.UTF8, "application/json");
-        }
+        //private StringContent CreateJsonContent(object obj)
+        //{
+        //    var json = JsonConvert.SerializeObject(obj);
+        //    return new StringContent(json, Encoding.UTF8, "application/json");
+        //}
     }
 }
